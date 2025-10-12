630081307434dabce069a5a954225d33a2235680
Wiktor Koncewicz <wiktor.koncewicz31a@gmail.com>
2025-10-12 18:04:44 +0200

    Contenerization and basic backend, frontend and db

---
+++ frontend/app/src/Page.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+const url = process.env.REACT_APP_BACKEND_URL;
+
+function Page() {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(url + '/message');
+                setMessage(res.data.message);
+            } catch (error) {
+                console.error('Error fetching posts:', error.message);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div className="p-8 space-y-6 bg-gray-100">
+            <h1>Welcome to our page!</h1>
+            <div>This is a message from our backend: {message}</div>
+        </div>
+    );
+}
+
+export default Page;
---
+++ frontend/app/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Ensures Tailwind looks through all your files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
