bae769a1272d5ebd82ce009da6e60e9dbedb0638
Wiktor Koncewicz <wiktor.koncewicz31a@gmail.com>
2025-10-14 19:10:38 +0200

    Move to python backend

---
+++ backend/app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/message', methods=['GET'])
+def message():
+    return jsonify({"message": "Hello world"}), 200
+
+if __name__ == '__main__':
+    app.run(port=5000)
