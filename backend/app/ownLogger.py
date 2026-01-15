d95b20d0a02f3236c56508bc83623edbdd3852d2
Milan Kwiatkowski <mkwiatkowski@inf.ug.edu.pl>
2026-01-15 10:56:46 +0100

    Remade logging with built-in logger

--- backend/app/ownLogger.py
+++ backend/app/ownLogger.py
@@ -1,9 +1,21 @@
 from datetime import datetime, timezone
 from mongo_models import logs
-def getTime():
-    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return time
-def saveLog(service,type,message,transactionId):
-    log = logs.Logs(service=service,type=type,time=getTime(),message=message,transactionId=transactionId)
-    log.save()
-    print(f'{service} {type} {message} transactionId: {transactionId} {getTime()}')
+import logging
+logger = logging.getLogger("app")
+
+def saveLog(service,level,message,transactionId):
+    if(level=='INFO'):
+        logger.info(message,extra={'service':service,'transactionId':transactionId})
+    elif(level=='ERROR'):
+        logger.error(message,extra={'service':service,'transactionId':transactionId})
+    elif(level='WARNING'):
+        logger.warning(message,extra={'service':service,'transactionId':transactionId})
+    try:
+        logs.Log(
+            service=service,
+            time = datetime.now(timezone.utc)
+            message=message,
+            transactionId=transactionId
+        )
+    except Exception as e:
+        logger.error(f"An error occured while tryind to save log for transactionId {transactionId}")
