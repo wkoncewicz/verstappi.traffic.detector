a1b3f4720ece4df8eb63b7d6eb291f1259929950
milankwiatkowski <milan.mikolaj.kwiatkowski@gmail.com>
2026-01-19 23:05:58 +0100

    readded kcProvider wrapper

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import HlsPlayer from "@/components/HlsPlayer";
+import KeycloakProvider from "@/components/kcprovider";
 
 export default function RootLayout({
   children,
@@ -16,7 +17,7 @@ export default function RootLayout({
             <div style={{ padding: 24, maxWidth:720 }}>
               <HlsPlayer src="https://wzmedia.dot.ca.gov/D3/99_JCT162E_BUT99_NB.stream/chunklist_w646513265.m3u8" />
             </div>
-          {children}
+          <KeycloakProvider>{children}</KeycloakProvider>
         </main>
         <Footer />
       </body>
